bbdce5e15643916a61eec530b0341008ae9a651e
Antoine Lambert <anlambert@softwareheritage.org>
2024-04-05 11:40:57 +0200

    test_scrubber: Add new test checking missing contents are reported

--- tests/test_scrubber.py
+++ tests/test_scrubber.py
@@ -17,49 +17,140 @@ def compose_files() -> List[str]:
     return ["compose.yml", "compose.scrubber.yml"]
 
 
-def test_scrubber_check(docker_compose, origins):
+@pytest.fixture(autouse=True)
+def scrubber_service_init(docker_compose):
     # start the scrubber service (the compose file does not start it up, it
     # just defines it)
     docker_compose.check_compose_output(
         "up --scale swh-scrubber=1  --no-recreate --wait swh-scrubber"
     )
-    scrubber = compose_host_for_service(docker_compose, "swh-scrubber")
 
-    # configure a scrubbing session for snapshots using 16 partitions
-    scrubber.check_output(
-        "swh scrubber check init storage "
-        "--object-type snapshot "
-        "--nb-partitions 16 "
-        "--name snapshot_4 "
+
+@pytest.fixture
+def scrubber_service(docker_compose):
+    return compose_host_for_service(docker_compose, "swh-scrubber")
+
+
+@pytest.fixture
+def storage_service(docker_compose):
+    return compose_host_for_service(docker_compose, "swh-storage")
+
+
+def create_scrubber_config(
+    scrubber_service, backend, obj_type, nb_partitions=1, check_references=False
+):
+    print(f"Creating the scrubber config for checking objects of type {obj_type}")
+    config_name = f"{obj_type}_{nb_partitions}"
+    check_init_cmd = (
+        f"swh scrubber check init {backend} --object-type {obj_type} "
+        f"--nb-partitions {nb_partitions} --name {config_name}"
     )
+    if check_references:
+        check_init_cmd += " --check-references"
+    output = scrubber_service.check_output(check_init_cmd)
+    assert output.startswith(f"Created configuration {config_name}")
+    return config_name
 
+
+def run_scrubber_with_config(
+    scrubber_service, backend, config_name, obj_type, nb_partitions=None
+):
     # check we have the expected scrubbing configuration registered
-    expected = (
-        "[1] snapshot_4: snapshot, 16, storage:postgresql "
-        "(postgresql:///?service=swh-storage)"
+    cfg_lst = scrubber_service.check_output("swh scrubber check list")
+    assert f"{config_name}: {obj_type}, {nb_partitions}" in cfg_lst, cfg_lst
+
+    print(f"Starting a SWH {backend} scrubber with config {config_name}")
+    scrubber_service.check_output(f"swh scrubber check run {config_name}")
+
+    # return scrubbing stats
+    return json.loads(
+        scrubber_service.check_output(f"swh scrubber check stats {config_name} -j")
     )
-    cfg_lst = scrubber.check_output("swh scrubber check list")
-    assert expected in cfg_lst, cfg_lst
 
-    # run the scrubbing session using one worker
-    scrubber.check_output("swh scrubber check storage snapshot_4")
 
-    # check the scrubbing session has checked all the snapshot partitions
-    stats = json.loads(scrubber.check_output("swh scrubber check stats snapshot_4 -j"))
+def test_storage_scrubber_check_snapshots_no_corruption(scrubber_service, origins):
+    """Run storage scrubbing on non corrupted neither missing snapshot objects."""
 
-    assert stats["config"]["name"] == "snapshot_4"
-    assert stats["config"]["object_type"] == "snapshot"
-    assert stats["config"]["nb_partitions"] == 16
+    obj_type = "snapshot"
+    nb_partitions = 16
 
-    assert stats["checked_partition"] == 16
+    config_name = create_scrubber_config(
+        scrubber_service,
+        backend="storage",
+        obj_type=obj_type,
+        nb_partitions=nb_partitions,
+    )
+
+    stats = run_scrubber_with_config(
+        scrubber_service,
+        backend="storage",
+        config_name=config_name,
+        obj_type=obj_type,
+        nb_partitions=nb_partitions,
+    )
+
+    assert stats["config"]["name"] == config_name
+    assert stats["config"]["object_type"] == obj_type
+    assert stats["config"]["nb_partitions"] == nb_partitions
+
+    assert stats["checked_partition"] == nb_partitions
     assert stats["corrupt_object"] == 0
     assert stats["missing_object"] == 0
 
-    # alter the content of the storage...
-    # xx
 
-    # reset the scrubbing session and rerun it
-    # xx
+@pytest.fixture
+def deleted_contents(storage_service):
 
-    # check the stats for found errors
-    # xx
+    def run_storage_sql_query(query):
+        return storage_service.check_output(f'psql service=swh-storage -t -c "{query}"')
+
+    nb_contents = int(run_storage_sql_query("SELECT count(*) FROM content"))
+
+    assert nb_contents
+
+    run_storage_sql_query("CREATE TABLE content_cp AS TABLE content")
+
+    # remove all contents from storage
+    run_storage_sql_query("TRUNCATE content")
+
+    yield nb_contents
+
+    run_storage_sql_query("INSERT INTO content (SELECT * FROM content_cp)")
+    run_storage_sql_query("DROP TABLE content_cp")
+
+
+def test_storage_scrubber_check_directory_missing_contents(
+    scrubber_service,
+    origins,
+    deleted_contents,
+):
+    """Run storage scrubbing on directories referencing missing content objects."""
+
+    obj_type = "directory"
+    nb_partitions = 16
+
+    # configure and run scrubbing
+    config_name = create_scrubber_config(
+        scrubber_service,
+        backend="storage",
+        obj_type=obj_type,
+        nb_partitions=nb_partitions,
+        check_references=True,
+    )
+
+    stats = run_scrubber_with_config(
+        scrubber_service,
+        backend="storage",
+        config_name=config_name,
+        obj_type=obj_type,
+        nb_partitions=nb_partitions,
+    )
+
+    # check missing content objects were reported
+    assert stats["config"]["name"] == config_name
+    assert stats["config"]["object_type"] == obj_type
+    assert stats["config"]["nb_partitions"] == nb_partitions
+
+    assert stats["checked_partition"] == nb_partitions
+    assert stats["corrupt_object"] == 0
+    assert stats["missing_object"] == deleted_contents
