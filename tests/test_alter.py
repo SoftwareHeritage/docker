SoftwareHeritage/docker

commit d3729924dd932ccdf2e28426f4957b51a1869ab5

Use a common config file name and declare the entrypoint in the image
Put the config for all swh services in /srv/softwareheritage/config.yml;
put the entrypoint.sh script in this directory, and make this later the
default working directory.

This allows to remove many useless config files from the compose files.

tests/test_alter.py
@@ -223,7 +223,7 @@ def test_fork_removed_in_kafka(docker_compose, fork_removed):
 def fork_restored(fork_removed, alter_host, docker_compose, kafka_api_url):
     alter_host.check_output(
         f"swh alter recovery-bundle restore '{fork_removed.bundle_path}' "
-        "--identity /age-identities.txt"
+        "--identity /srv/softwareheritage/age-identities.txt"
     )
     wait_for_replayer(docker_compose, kafka_api_url)
     return fork_removed

Dockerfile
@@ -115,3 +115,6 @@ COPY utils/*.sh /srv/softwareheritage/utils/
 RUN mkdir -p /srv/softwareheritage/objects
 RUN mkdir -p /srv/softwareheritage/graph
 RUN rm -rd /srv/softwareheritage/.cache
+WORKDIR /srv/softwareheritage/
+ENV SWH_CONFIG_FILENAME=/srv/softwareheritage/config.yml
+ENTRYPOINT ["/srv/softwareheritage/entrypoint.sh"]

compose.alter.yml
@@ -4,15 +4,12 @@ services:
   swh-alter:
     image: swh/stack
     build: ./
-    environment:
-      SWH_CONFIG_FILENAME: /alter.yml
-    entrypoint: /entrypoint.sh
     command: mock-graph
     volumes:
       - "./tests/alter_companion.py:/src/alter_companion.py:ro"
-      - "./services/swh-alter/entrypoint.sh:/entrypoint.sh:ro"
-      - "./conf/alter.yml:/alter.yml:ro"
-      - "./conf/alter/age-identities.txt:/age-identities.txt:ro"
+      - "./services/swh-alter/entrypoint.sh:/srv/softwareheritage/entrypoint.sh:ro"
+      - "./conf/alter.yml:/srv/softwareheritage/config.yml:ro"
+      - "./conf/alter/age-identities.txt:/srv/softwareheritage/age-identities.txt:ro"
 
   cassandra-seed:
     # This container starts a Cassandra instance that must be used as the
@@ -52,15 +49,13 @@ services:
     env_file:
       - ./env/common_python.env
     environment:
-      SWH_CONFIG_FILENAME: /storage.yml
       RPC_PORT: 5002
       CASSANDRA_SEED: cassandra-seed
       PYTHONUNBUFFERED: 1
-    entrypoint: /entrypoint.sh
     command: rpc
     volumes:
-      - "./conf/alter/storage_cassandra.yml:/storage.yml:ro"
-      - "./services/swh-storage/entrypoint.sh:/entrypoint.sh:ro"
+      - "./conf/alter/storage_cassandra.yml:/srv/softwareheritage/config.yml:ro"
+      - "./services/swh-storage/entrypoint.sh:/srv/softwareheritage/entrypoint.sh:ro"
 
   swh-extra-objstorage:
     build: ./
@@ -70,16 +65,14 @@ services:
     env_file:
       - ./env/common_python.env
     environment:
-      SWH_CONFIG_FILENAME: /objstorage.yml
       RPC_PORT: 5003
-    entrypoint: /entrypoint.sh
     command: rpc
     healthcheck:
       test: curl -f http://localhost:5003 || exit 1
       retries: 6
     volumes:
-      - "./conf/objstorage.yml:/objstorage.yml:ro"
-      - "./services/swh-objstorage/entrypoint.sh:/entrypoint.sh:ro"
+      - "./conf/objstorage.yml:/srv/softwareheritage/config.yml:ro"
+      - "./services/swh-objstorage/entrypoint.sh:/srv/softwareheritage/entrypoint.sh:ro"
 
   swh-storage-replayer:
     image: swh/stack
@@ -91,13 +84,10 @@ services:
       - prometheus-statsd-exporter
     env_file:
       - ./env/common_python.env
-    environment:
-      SWH_CONFIG_FILENAME: /replayer.yml
-    entrypoint: /entrypoint.sh
     command: replayer
     volumes:
-      - "./conf/alter/replayer.yml:/replayer.yml:ro"
-      - "./services/swh-storage/entrypoint.sh:/entrypoint.sh:ro"
+      - "./conf/alter/replayer.yml:/srv/softwareheritage/config.yml:ro"
+      - "./services/swh-storage/entrypoint.sh:/srv/softwareheritage/entrypoint.sh:ro"
 
   swh-objstorage-replayer:
     image: swh/stack
@@ -110,10 +100,7 @@ services:
       - nginx
     env_file:
       - ./env/common_python.env
-    environment:
-      SWH_CONFIG_FILENAME: /content-replayer.yml
-    entrypoint: /entrypoint.sh
     command: replayer
     volumes:
-      - "./conf/alter/content-replayer.yml:/content-replayer.yml:ro"
-      - "./services/swh-objstorage/entrypoint.sh:/entrypoint.sh:ro"
+      - "./conf/alter/content-replayer.yml:/srv/softwareheritage/config.yml:ro"
+      - "./services/swh-objstorage/entrypoint.sh:/srv/softwareheritage/entrypoint.sh:ro"

compose.cassandra.yml
@@ -44,8 +44,8 @@ services:
 
   swh-storage:
     volumes:
-      - "./conf/storage_cassandra.yml:/storage.yml:ro"
-      - "./services/swh-storage/entrypoint.sh:/entrypoint.sh:ro"
+      - "./conf/storage_cassandra.yml:/srv/softwareheritage/config.yml:ro"
+      - "./services/swh-storage/entrypoint.sh:/srv/softwareheritage/entrypoint.sh:ro"
     depends_on:
       # beware swh-storage-db will remains a dependency; since compose does not
       # support !reset with replacement value, we cannot easily remove this

compose.deposit.yml
@@ -22,16 +22,14 @@ services:
     environment:
       POSTGRES_DB: swh-deposit
       VERBOSITY: 3
-      SWH_CONFIG_FILENAME: /deposit.yml
       DJANGO_SETTINGS_MODULE: swh.deposit.settings.production
       RPC_PORT: 5006
-    entrypoint: /entrypoint.sh
     healthcheck:
       test: curl -f http://localhost:5006 || exit 1
       retries: 6
     volumes:
-      - "./conf/deposit.yml:/deposit.yml:ro"
-      - "./services/swh-deposit/entrypoint.sh:/entrypoint.sh:ro"
+      - "./conf/deposit.yml:/srv/softwareheritage/config.yml:ro"
+      - "./services/swh-deposit/entrypoint.sh:/srv/softwareheritage/entrypoint.sh:ro"
 
   swh-loader-deposit:
     image: swh/stack
@@ -42,9 +40,7 @@ services:
     user: swh
     environment:
       SWH_WORKER_INSTANCE: loader-deposit
-      SWH_CONFIG_FILENAME: /loader-deposit.yml
       SWH_SCHEDULER_INSTANCE: http://nginx/scheduler
-    entrypoint: /entrypoint.sh
     command: worker
     depends_on:
       - swh-storage
@@ -54,5 +50,5 @@ services:
       - prometheus-statsd-exporter
       - nginx
     volumes:
-      - "./conf/loader-deposit.yml:/loader-deposit.yml:ro"
-      - "./services/swh-worker/entrypoint.sh:/entrypoint.sh:ro"
+      - "./conf/loader-deposit.yml:/srv/softwareheritage/config.yml:ro"
+      - "./services/swh-worker/entrypoint.sh:/srv/softwareheritage/entrypoint.sh:ro"

compose.graph.yml
@@ -4,19 +4,15 @@ services:
   swh-graph:
     image: swh/stack
     build: ./
-    entrypoint: /entrypoint.sh
-    environment:
-      SWH_CONFIG_FILENAME: /graph.yml
     depends_on:
       - kafka
       - prometheus-statsd-exporter
     env_file:
       - ./env/common_python.env
     volumes:
-      - "./conf/graph.yml:/graph.yml:ro"
-      - "./services/swh-graph/entrypoint.sh:/entrypoint.sh:ro"
+      - "./conf/graph.yml:/srv/softwareheritage/config.yml:ro"
+      - "./services/swh-graph/entrypoint.sh:/srv/softwareheritage/entrypoint.sh:ro"
       - swh-graph-data:/srv/softwareheritage/graph
-
 
 volumes:
   swh-graph-data:

compose.keycloak.yml
@@ -19,7 +19,7 @@ services:
       JAVA_TOOL_OPTIONS: "-Dkeycloak.profile=preview
                           -Dkeycloak.profile.feature.token_exchange=enabled"
     volumes:
-      - "./services/keycloak/entrypoint.sh:/entrypoint.sh:ro"
+      - "./services/keycloak/entrypoint.sh:/srv/softwareheritage/entrypoint.sh:ro"
       - "./services/keycloak/keycloak_swh_setup.py:/keycloak_swh_setup.py:ro"
     expose:
       - "8080"
@@ -29,24 +29,19 @@ services:
   swh-web:
     depends_on:
       - keycloak
-    environment:
-      SWH_CONFIG_FILENAME: /web-keycloak.yml
     volumes:
-      - "./conf/web-keycloak.yml:/web-keycloak.yml:ro"
+      - "./conf/web-keycloak.yml:/srv/softwareheritage/config.yml:ro"
 
   swh-deposit:
     depends_on:
       - keycloak
     environment:
-      SWH_CONFIG_FILENAME: /deposit-keycloak.yml
       MEMCACHED: "yes"
     volumes:
-      - "./conf/deposit-keycloak.yml:/deposit-keycloak.yml:ro"
+      - "./conf/deposit-keycloak.yml:/srv/softwareheritage/config.yml:ro"
 
   swh-graphql:
     depends_on:
       - keycloak
-    environment:
-      SWH_CONFIG_FILENAME: /graphql-keycloak.yml
     volumes:
-      - "./conf/graphql-keycloak.yml:/graphql-keycloak.yml:ro"
+      - "./conf/graphql-keycloak.yml:/srv/softwareheritage/config.yml:ro"

compose.mirror.yml
@@ -31,16 +31,14 @@ services:
       DB_FLAVOR: mirror
       POSTGRES_DB: swh-mirror-storage
       STORAGE_BACKEND: postgresql
-      SWH_CONFIG_FILENAME: /storage.yml
       RPC_PORT: 5002
-    entrypoint: /entrypoint.sh
     command: rpc
     healthcheck:
       test: curl -f http://localhost:5002 || exit 1
       retries: 6
     volumes:
-      - "./conf/mirror/storage.yml:/storage.yml:ro"
-      - "./services/swh-storage/entrypoint.sh:/entrypoint.sh:ro"
+      - "./conf/mirror/storage.yml:/srv/softwareheritage/config.yml:ro"
+      - "./services/swh-storage/entrypoint.sh:/srv/softwareheritage/entrypoint.sh:ro"
 
   # Object storage
   swh-mirror-objstorage:
@@ -51,16 +49,14 @@ services:
     depends_on:
       - prometheus-statsd-exporter
     environment:
-      SWH_CONFIG_FILENAME: /objstorage.yml
       RPC_PORT: 5003
-    entrypoint: /entrypoint.sh
     command: rpc
     healthcheck:
       test: curl -f http://localhost:5003 || exit 1
       retries: 6
     volumes:
-      - "./conf/objstorage.yml:/objstorage.yml:ro"
-      - "./services/swh-objstorage/entrypoint.sh:/entrypoint.sh:ro"
+      - "./conf/objstorage.yml:/srv/softwareheritage/config.yml:ro"
+      - "./services/swh-objstorage/entrypoint.sh:/srv/softwareheritage/entrypoint.sh:ro"
       - /srv/softwareheritage/objects
 
   # a dedicated swh-web db
@@ -87,14 +83,12 @@ services:
       POSTGRES_DB: swh-mirror-web
       VERBOSITY: 3
       DJANGO_SETTINGS_MODULE: swh.web.settings.production
-      SWH_CONFIG_FILENAME: /web.yml
-    entrypoint: /entrypoint.sh
     healthcheck:
       test: curl -f http://localhost:5004 || exit 1
       retries: 6
     volumes:
-      - "./conf/mirror/web.yml:/web.yml:ro"
-      - "./services/swh-web/entrypoint.sh:/entrypoint.sh:ro"
+      - "./conf/mirror/web.yml:/srv/softwareheritage/config.yml:ro"
+      - "./services/swh-web/entrypoint.sh:/srv/softwareheritage/entrypoint.sh:ro"
       - "./assets/mirror/:/mirror/:ro"
 
   # and the background processes that keeps the mirror in sync with the
@@ -110,13 +104,11 @@ services:
     env_file:
       - ./env/common_python.env
     environment:
-      SWH_CONFIG_FILENAME: /replayer.yml
       LOG_LEVEL: DEBUG
-    entrypoint: /entrypoint.sh
     command: replayer
     volumes:
-      - "./conf/mirror/replayer.yml:/replayer.yml:ro"
-      - "./services/swh-storage/entrypoint.sh:/entrypoint.sh:ro"
+      - "./conf/mirror/replayer.yml:/srv/softwareheritage/config.yml:ro"
+      - "./services/swh-storage/entrypoint.sh:/srv/softwareheritage/entrypoint.sh:ro"
     deploy:
        mode: replicated
        replicas: 2
@@ -134,13 +126,11 @@ services:
     env_file:
       - ./env/common_python.env
     environment:
-      SWH_CONFIG_FILENAME: /content-replayer.yml
       LOG_LEVEL: DEBUG
-    entrypoint: /entrypoint.sh
     command: replayer
     volumes:
-      - "./conf/mirror/content-replayer.yml:/content-replayer.yml:ro"
-      - "./services/swh-objstorage/entrypoint.sh:/entrypoint.sh:ro"
+      - "./conf/mirror/content-replayer.yml:/srv/softwareheritage/config.yml:ro"
+      - "./services/swh-objstorage/entrypoint.sh:/srv/softwareheritage/entrypoint.sh:ro"
     deploy:
        mode: replicated
        replicas: 4
@@ -161,9 +151,7 @@ services:
       - ./env/common_python.env
     environment:
       POSTGRES_DB: swh-mirror-vault
-      SWH_CONFIG_FILENAME: /vault.yml
       RPC_PORT: 5005
-    entrypoint: /entrypoint.sh
     command: rpc
     healthcheck:
       test: curl -f http://localhost:5005 || exit 1
@@ -176,19 +164,17 @@ services:
       - prometheus-statsd-exporter
       - nginx-mirror
     volumes:
-      - "./conf/mirror/vault.yml:/vault.yml:ro"
-      - "./services/swh-vault/entrypoint.sh:/entrypoint.sh:ro"
+      - "./conf/mirror/vault.yml:/srv/softwareheritage/config.yml:ro"
+      - "./services/swh-vault/entrypoint.sh:/srv/softwareheritage/entrypoint.sh:ro"
 
   swh-mirror-vault-worker:
     image: swh/stack
     build: ./
-    entrypoint: /entrypoint.sh
     command: worker
     env_file:
       - ./env/common_python.env
       - ./env/workers.env
     environment:
-      SWH_CONFIG_FILENAME: /cooker.yml
       SWH_SCHEDULER_HOST: swh-mirror-scheduler
       SWH_SCHEDULER_INSTANCE: http://nginx-mirror/scheduler
     depends_on:
@@ -197,8 +183,8 @@ services:
       - prometheus-statsd-exporter
       - nginx-mirror
     volumes:
-      - "./conf/mirror/vault-worker.yml:/cooker.yml:ro"
-      - "./services/swh-vault/entrypoint.sh:/entrypoint.sh:ro"
+      - "./conf/mirror/vault-worker.yml:/srv/softwareheritage/config.yml:ro"
+      - "./services/swh-vault/entrypoint.sh:/srv/softwareheritage/entrypoint.sh:ro"
 
   swh-mirror-scheduler-db:
     image: postgres:16
@@ -215,11 +201,11 @@ services:
       - ./env/common_python.env
     environment:
       POSTGRES_DB: swh-mirror-scheduler
-      SWH_CONFIG_FILENAME: /scheduler.yml
-      SWH_SCHEDULER_CONFIG_FILE: /scheduler.yml
+      # this is required because currently the 'swh scheduler' does not honor
+      # the SWH_CONFIG_FILENAME env var...
+      SWH_SCHEDULER_CONFIG_FILE: /srv/softwareheritage/config.yml
       RPC_PORT: 5008
       BROKER_URL: amqp://amqp-mirror/
-    entrypoint: /entrypoint.sh
     command: rpc
     healthcheck:
       test: curl -f http://localhost:5008 || exit 1
@@ -229,8 +215,8 @@ services:
       - amqp-mirror
       - prometheus-statsd-exporter
     volumes:
-      - "./conf/mirror/scheduler.yml:/scheduler.yml:ro"
-      - "./services/swh-scheduler/entrypoint.sh:/entrypoint.sh:ro"
+      - "./conf/mirror/scheduler.yml:/srv/softwareheritage/config.yml:ro"
+      - "./services/swh-scheduler/entrypoint.sh:/srv/softwareheritage/entrypoint.sh:ro"
 
   swh-mirror-scheduler-listener:
     image: swh/stack
@@ -239,20 +225,18 @@ services:
       - ./env/common_python.env
     environment:
       POSTGRES_DB: swh-mirror-scheduler
-      SWH_CONFIG_FILENAME: /scheduler.yml
-      SWH_SCHEDULER_CONFIG_FILE: /scheduler.yml
+      SWH_SCHEDULER_CONFIG_FILE: /srv/softwareheritage/config.yml
       BROKER_URL: amqp://amqp-mirror/
       WORKER_INSTANCES: vault
       APP: swh.scheduler.celery_backend.config.app
-    entrypoint: /entrypoint.sh
     command: worker start-listener
     depends_on:
       - swh-mirror-scheduler
       - amqp-mirror
       - prometheus-statsd-exporter
     volumes:
-      - "./conf/mirror/scheduler.yml:/scheduler.yml:ro"
-      - "./services/swh-scheduler/entrypoint.sh:/entrypoint.sh:ro"
+      - "./conf/mirror/scheduler.yml:/srv/softwareheritage/config.yml:ro"
+      - "./services/swh-scheduler/entrypoint.sh:/srv/softwareheritage/entrypoint.sh:ro"
 
   swh-mirror-scheduler-runner:
     image: swh/stack
@@ -261,20 +245,18 @@ services:
       - ./env/common_python.env
     environment:
       POSTGRES_DB: swh-mirror-scheduler
-      SWH_CONFIG_FILENAME: /scheduler.yml
-      SWH_SCHEDULER_CONFIG_FILE: /scheduler.yml
+      SWH_SCHEDULER_CONFIG_FILE: /srv/softwareheritage/config.yml
       BROKER_URL: amqp://amqp-mirror/
       WORKER_INSTANCES: vault
       APP: swh.scheduler.celery_backend.config.app
-    entrypoint: /entrypoint.sh
     command: worker start-runner --period 10
     depends_on:
       - swh-mirror-scheduler
       - amqp-mirror
       - prometheus-statsd-exporter
     volumes:
-      - "./conf/mirror/scheduler.yml:/scheduler.yml:ro"
-      - "./services/swh-scheduler/entrypoint.sh:/entrypoint.sh:ro"
+      - "./conf/mirror/scheduler.yml:/srv/softwareheritage/config.yml:ro"
+      - "./services/swh-scheduler/entrypoint.sh:/srv/softwareheritage/entrypoint.sh:ro"
 
   amqp-mirror:
     image: rabbitmq:3.6-management

commit e9b5afcf2d762d0539df9a5e44029f61aa35d08e

alter: fix RemovalOperation.get_removed_content_sha1s
Commit d3729924dd broke the extraction of Content data from the recovery
bundles. On top of fixing the root cause which was a wrong path to
the identity file, let’s add some debug logs and sanity checks on the
command results to avoid always computing a SHA1 value on an empty string.

tests/test_alter.py
@@ -5,6 +5,7 @@
 
 import dataclasses
 import hashlib
+import logging
 import random
 from typing import Iterable, List, Optional, Tuple
 
@@ -14,6 +15,8 @@ import testinfra
 import yaml
 
 from .utils import retry_until_success
+
+logger = logging.getLogger(__name__)
 
 
 @pytest.fixture(scope="module")
@@ -150,13 +153,18 @@ class RemovalOperation:
                 ],
                 k=5,
             )
+            logger.debug(
+                "random content sha1s picked %s", ", ".join(some_content_swhids)
+            )
             for swhid in some_content_swhids:
-                content = host.run(
+                cmd = host.run(
                     f"swh alter recovery-bundle extract-content "
-                    "--identity /age-identities.txt --output - "
+                    "--identity /srv/softwareheritage/age-identities.txt --output - "
                     f"'{self.bundle_path}' '{swhid}'"
-                ).stdout_bytes
-                sha1 = hashlib.sha1(content).hexdigest()
+                )
+                assert cmd.succeeded, f"extract-content failed! {cmd.stderr}"
+                sha1 = hashlib.sha1(cmd.stdout_bytes).hexdigest()
+                logger.debug("%s data SHA1: %s", swhid, sha1)
                 self._removed_content_sha1s.append(sha1)
         return self._removed_content_sha1s
 