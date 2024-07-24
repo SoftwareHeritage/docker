0e7ffbb833cee742ea5a4c0416baff51836abc40
Jérémy Bobbio (Lunar) <lunar@softwareheritage.org>
2024-07-24 14:33:24 +0200

    Improve tests for swh-alter
    The semantic of the tests assumed that swh-alter had been foked from
    swh-py-template. This was not true as files were copied instead. We now
    use the main `swh-py-template` repository and `lunar/swh-py-template`.
    
    We now ensure–by checking removed swhids and swhids referenced by the
    removal set-that the “Marking candidates used elsewhere” step of the
    selection algorithm work. This would not have been the case without
    the `record_references` storage proxy configured in c6cf71e38e.
    
    The objstorage tests that were done on the fork now has to be done on
    the initial repository to ensure a sample of Content objects big enough
    to matter.
    
    We also test what happens when we remove all origins at the same time.

--- tests/test_alter.py
+++ tests/test_alter.py
@@ -51,7 +51,7 @@ def compose_services() -> List[str]:
 def origin_urls() -> List[Tuple[str, str]]:
     return [
         ("git", "https://gitlab.softwareheritage.org/swh/devel/swh-py-template.git"),
-        ("git", "https://gitlab.softwareheritage.org/swh/devel/swh-alter.git"),
+        ("git", "https://gitlab.softwareheritage.org/lunar/swh-py-template.git"),
     ]
 
 
@@ -137,6 +137,7 @@ class RemovalOperation:
     bundle_path: str
     origins: List[str]
     removed_swhids: List[str] = dataclasses.field(default_factory=list)
+    referencing: List[str] = dataclasses.field(default_factory=list)
     _removed_content_sha1s: Optional[List[bytes]] = None
 
     def get_removed_content_sha1s(self, host):
@@ -175,18 +176,22 @@ class RemovalOperation:
             f"--recovery-bundle '{self.bundle_path}' "
             f"{' '.join(self.origins)}"
         )
-        print(remove_output)
+        logger.debug(remove_output)
         dump = host.check_output(
             f"swh alter recovery-bundle info --dump-manifest '{self.bundle_path}'"
         )
         manifest = yaml.safe_load(dump)
+        logger.debug("%s manifest %s", self.identifier, manifest)
         self.removed_swhids = manifest["swhids"]
+        if int(manifest["version"]) >= 3:
+            assert self.origins == manifest["requested"]
+            self.referencing = manifest["referencing"]
 
 
 FORK_REMOVAL_OP = RemovalOperation(
-    identifier="integration-test-01",
-    bundle_path="/tmp/integration-test-01.swh-recovery-bundle",
-    origins=["https://gitlab.softwareheritage.org/swh/devel/swh-alter.git"],
+    identifier="integration-test-fork",
+    bundle_path="/tmp/integration-test-fork.swh-recovery-bundle",
+    origins=["https://gitlab.softwareheritage.org/lunar/swh-py-template.git"],
 )
 
 
@@ -202,24 +207,6 @@ def test_fork_removed_in_postgresql(docker_compose, fork_removed):
     docker_compose.check_compose_output(
         "exec swh-alter python /src/alter_companion.py "
         f"query-postgresql {' '.join(fork_removed.removed_swhids)}"
-    )
-
-
-def test_fork_removed_in_primary_objstorage(docker_compose, fork_removed, alter_host):
-    # Ensure objects have been removed from primary objstorage
-    docker_compose.check_compose_output(
-        "exec swh-alter python /src/alter_companion.py query-objstorage "
-        "--objstorage-url http://nginx/rpc/objstorage "
-        f"{' '.join(fork_removed.get_removed_content_sha1s(alter_host))}"
-    )
-
-
-def test_fork_removed_in_extra_objstorage(docker_compose, fork_removed, alter_host):
-    # Ensure objects have been removed from extra objstorage
-    docker_compose.check_compose_output(
-        "exec swh-alter python /src/alter_companion.py query-objstorage "
-        "--objstorage-url http://swh-extra-objstorage:5003 "
-        f"{' '.join(fork_removed.get_removed_content_sha1s(alter_host))}"
     )
 
 
@@ -265,24 +252,6 @@ def test_fork_restored_in_postgresql(docker_compose, fork_restored):
     )
 
 
-def test_fork_restored_in_primary_objstorage(docker_compose, fork_restored, alter_host):
-    # Ensure objects are back in primary objstorage
-    docker_compose.check_compose_output(
-        "exec swh-alter python /src/alter_companion.py query-objstorage --presence "
-        "--objstorage-url http://nginx/rpc/objstorage "
-        f"{' '.join(fork_restored.get_removed_content_sha1s(alter_host))}"
-    )
-
-
-def test_fork_restored_in_extra_objstorage(docker_compose, fork_restored, alter_host):
-    # Ensure objects are back in extra objstorage (through the replayer)
-    docker_compose.check_compose_output(
-        "exec swh-alter python /src/alter_companion.py query-objstorage --presence "
-        "--objstorage-url http://swh-extra-objstorage:5003 "
-        f"{' '.join(fork_restored.get_removed_content_sha1s(alter_host))}"
-    )
-
-
 def test_fork_restored_in_cassandra(docker_compose, fork_restored):
     # Ensure the SWHIDs are back in Cassandra (through the replayer)
     docker_compose.check_compose_output(
@@ -305,3 +274,133 @@ def test_fork_restored_in_elasticsearch(docker_compose, fork_restored):
         "exec swh-alter python /src/alter_companion.py "
         f"query-elasticsearch --presence {' '.join(fork_restored.origins)}"
     )
+
+
+INITIAL_REMOVAL_OP = RemovalOperation(
+    identifier="integration-test-initial",
+    bundle_path="/tmp/integration-test-initial.swh-recovery-bundle",
+    origins=["https://gitlab.softwareheritage.org/swh/devel/swh-py-template.git"],
+)
+
+
+@pytest.fixture(scope="module")
+def initial_removed(alter_host, verified_origins, fork_restored):
+    INITIAL_REMOVAL_OP.run_in(alter_host)
+    assert len(INITIAL_REMOVAL_OP.removed_swhids) > 0
+    return INITIAL_REMOVAL_OP
+
+
+def test_initial_removed_in_primary_objstorage(
+    docker_compose, initial_removed, alter_host
+):
+    # Ensure objects have been removed from primary objstorage
+    docker_compose.check_compose_output(
+        "exec swh-alter python /src/alter_companion.py query-objstorage "
+        "--objstorage-url http://nginx/rpc/objstorage "
+        f"{' '.join(initial_removed.get_removed_content_sha1s(alter_host))}"
+    )
+
+
+def test_initial_removed_in_extra_objstorage(
+    docker_compose, initial_removed, alter_host
+):
+    # Ensure objects have been removed from extra objstorage
+    docker_compose.check_compose_output(
+        "exec swh-alter python /src/alter_companion.py query-objstorage "
+        "--objstorage-url http://swh-extra-objstorage:5003 "
+        f"{' '.join(initial_removed.get_removed_content_sha1s(alter_host))}"
+    )
+
+
+def test_fork_restored_initial_removed_fork_still_available(
+    docker_compose, fork_restored, initial_removed
+):
+    # Please keep in mind that the removal operation for the fork was done
+    # while initial was in storage and, likewise, the removal operation for the
+    # initial project was done while the fork was in storage.
+    removed_with_fork = set(fork_restored.removed_swhids)
+    removed_with_initial = set(initial_removed.removed_swhids)
+    referencing_with_fork = set(fork_restored.referencing)
+    referencing_with_initial = set(initial_removed.referencing)
+    # As the fork has been restored, no objects belonging to the fork should
+    # have been removed:
+    assert removed_with_fork.isdisjoint(removed_with_initial)
+    # But they should be referencing objects from one another
+    assert len(referencing_with_initial - referencing_with_fork) > 0
+    assert len(referencing_with_fork - referencing_with_initial) > 0
+    # The GPLv3 should not have been removed (as it is listed as never
+    # removable in swh-alter) but referenced:
+    assert "swh:1:cnt:94a9ed024d3859793618152ea559a168bbcbb5e2" not in removed_with_fork
+    assert (
+        "swh:1:cnt:94a9ed024d3859793618152ea559a168bbcbb5e2" not in removed_with_initial
+    )
+    assert "swh:1:cnt:94a9ed024d3859793618152ea559a168bbcbb5e2" in referencing_with_fork
+    assert (
+        "swh:1:cnt:94a9ed024d3859793618152ea559a168bbcbb5e2" in referencing_with_initial
+    )
+
+
+@pytest.fixture(scope="module")
+def initial_restored(initial_removed, alter_host, docker_compose, kafka_api_url):
+    alter_host.check_output(
+        f"swh alter recovery-bundle restore '{initial_removed.bundle_path}' "
+        "--identity /srv/softwareheritage/age-identities.txt"
+    )
+    wait_for_replayer(docker_compose, kafka_api_url)
+    return initial_removed
+
+
+def test_initial_restored_in_primary_objstorage(
+    docker_compose, initial_restored, alter_host
+):
+    # Ensure objects are back in primary objstorage
+    docker_compose.check_compose_output(
+        "exec swh-alter python /src/alter_companion.py query-objstorage --presence "
+        "--objstorage-url http://nginx/rpc/objstorage "
+        f"{' '.join(initial_restored.get_removed_content_sha1s(alter_host))}"
+    )
+
+
+def test_initial_restored_in_extra_objstorage(
+    docker_compose, initial_restored, alter_host
+):
+    # Ensure objects are back in extra objstorage (through the replayer)
+    docker_compose.check_compose_output(
+        "exec swh-alter python /src/alter_companion.py query-objstorage --presence "
+        "--objstorage-url http://swh-extra-objstorage:5003 "
+        f"{' '.join(initial_restored.get_removed_content_sha1s(alter_host))}"
+    )
+
+
+BOTH_REMOVAL_OP = RemovalOperation(
+    identifier="integration-test-both",
+    bundle_path="/tmp/integration-test-both.swh-recovery-bundle",
+    origins=[
+        "https://gitlab.softwareheritage.org/swh/devel/swh-py-template.git",
+        "https://gitlab.softwareheritage.org/lunar/swh-py-template.git",
+    ],
+)
+
+
+@pytest.fixture(scope="module")
+def both_removed(alter_host, verified_origins, fork_restored, initial_restored):
+    BOTH_REMOVAL_OP.run_in(alter_host)
+    assert len(BOTH_REMOVAL_OP.removed_swhids) > 0
+    return BOTH_REMOVAL_OP
+
+
+def test_remove_both_forks(
+    docker_compose, fork_restored, initial_restored, both_removed
+):
+    removed_with_both = set(both_removed.removed_swhids)
+    assert removed_with_both.issuperset(set(fork_restored.removed_swhids))
+    assert removed_with_both.issuperset(set(initial_restored.removed_swhids))
+    # The only object referenced should be on the never removable list:
+    assert set(both_removed.referencing) == {
+        # Empty content
+        "swh:1:cnt:e69de29bb2d1d6434b8b29ae775ad8c2e48c5391",
+        # GPLv3
+        "swh:1:cnt:94a9ed024d3859793618152ea559a168bbcbb5e2",
+        # Directory with empty __init__.py
+        "swh:1:dir:9d1dcfdaf1a6857c5f83dc27019c7600e1ffaff8",
+    }
