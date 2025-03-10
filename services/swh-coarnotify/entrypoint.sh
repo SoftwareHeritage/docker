474ccb571305ea7c5a705e49503cc817d0b4434e
Renaud Boyer <renaud.boyer@sofwareheritage.org>
2025-03-10 14:34:53 +0100

    WIP start coarnotify in the stack

---
+++ services/swh-coarnotify/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -ex
+
+source /srv/softwareheritage/utils/pyutils.sh
+setup_pip
+
+source /srv/softwareheritage/utils/pgsql.sh
+setup_pgsql
+
+source /srv/softwareheritage/utils/swhutils.sh
+
+if [ "$1" = 'shell' ] ; then
+    shift
+    if (( $# == 0)); then
+        exec bash -i
+    else
+        "$@"
+    fi
+else
+    wait_pgsql
+
+    echo "Migrating db"
+    django-admin migrate --settings=${DJANGO_SETTINGS_MODULE}
+    swh_start_django
+fi
