8e12aaae37f1200dfdbd6701ba023f001b2bdaf2
Antoine Lambert <anlambert@softwareheritage.org>
2025-12-09 20:50:35 +0100

    tests: Optimize performance a bit
    Use direct loading of origins instead of scheduling them.
    
    Reduce number of needed compose services in tests as a consequence.

--- tests/test_alter.py
+++ tests/test_alter.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2023-2024  The Software Heritage developers
+# Copyright (C) 2023-2025  The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
@@ -34,12 +34,8 @@ def compose_services() -> List[str]:
         "swh-storage-replayer",
         "swh-extra-objstorage",
         "swh-objstorage-replayer",
-        "swh-scheduler-runner",
-        "swh-scheduler-listener",
-        "swh-scheduler-schedule-recurrent",
         "swh-web",
         "swh-loader",
-        "swh-lister",
     ]
 
 
